d7dbe3222bab130396761e1b64d52732e56b62d8
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-04-17 22:49:16 +0800

    add pat 1012 1036.

---
+++ pat/1036.cpp
@@ -0,0 +1,37 @@
+#include<cstdio>
+#include<cmath>
+#include<climits>
+#include <iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int nu;
+    char c;
+    cin >> nu >> c;
+    int col = nu, row = (int)(0.5 * nu);
+    if (0.5 * nu - row >= 0.5) ++row;
+    //第一行
+    for (int i = 0; i < col; ++i) cout << c;
+    cout << endl;
+    //中间
+    int mid = row - 2;
+    for (int i = 0; i < mid; ++i) {
+        cout << c;
+        for (int j = 0; j < col - 2; ++j) {
+            cout << ' ';
+        }
+        cout << c << endl;;
+    }
+    //最后一行
+    for (int i = 0; i < col; ++i) cout << c;
+    cout << endl;
+    return 0;
+}
