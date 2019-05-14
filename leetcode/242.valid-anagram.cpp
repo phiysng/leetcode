2a6d9f7bcdc6e9daf7c1fcca18a560a58d30e71e
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-05-14 19:15:18 +0800

    add leetcode 242 438.

---
+++ leetcode/242.valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        unordered_map<int, int> map_s, map_t;
+        for (char c : s)
+            map_s[c]++;
+        for (char c : t)
+            map_t[c]++;
+        return map_s == map_t;
+    }
+};
---
+++ leetcode/438.find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,87 @@
+class Solution
+{
+public:
+    vector<int> findAnagrams(string s, string p)
+    {
+        vector<int> res;
+        vector<int> map_p(26), map_s(26);
+        if (s.size() < p.size())
+            return {};
+        for (char c : p)
+            ++map_p[c - 'a'];
+
+        for (int i = 0; i < p.size(); ++i)
+        {
+            ++map_s[s[i] - 'a'];
+        }
+        if (map_p == map_s)
+            res.push_back(0);
+
+        for (int i = p.size(); i < s.size(); ++i)
+        {
+            ++map_s[s[i] - 'a'];
+            --map_s[s[i - p.size()] - 'a'];
+            if (map_s == map_p)
+                res.push_back(i - p.size() + 1);
+        }
+        return res;
+    }
+};
+
+///非常慢
+// class Solution
+// {
+// public:
+//     vector<int> findAnagrams(string s, string p)
+//     {
+//         if (s.size() < p.size())
+//             return {};
+//         vector<int> res;
+//         vector<int> map_t(26);
+//         init(p, map_t);
+//         unordered_map<string, int> cache;
+
+//         for (int i = 0; i < s.size() - p.size() + 1; ++i)
+//         {
+//             string t = s.substr(i, p.size());
+//             if (t == p)
+//             {
+//                 res.push_back(i);
+//                 continue;
+//             }
+//             if (cache[t] != 0)
+//             {
+//                 res.push_back(i);
+//                 continue;
+//             }
+//             if (isAnagram(t, map_t))
+//             {
+//                 res.push_back(i);
+//                 cache[t] = true;
+//             }
+//             else
+//             {
+//                 cache[t] = false;
+//             }
+//         }
+//         return res;
+//     }
+
+//     bool isAnagram(const string &s, vector<int> &map_t)
+//     {
+//         vector<int> map_s(26);
+
+//         for (char c : s)
+//             map_s[c - 'a']++;
+
+//         return map_s == map_t;
+//     }
+
+//     void init(const string &t, vector<int> &map_t)
+//     {
+//         for (char c : t)
+//             map_t[c - 'a']++;
+//     }
+
+// private:
+// };
