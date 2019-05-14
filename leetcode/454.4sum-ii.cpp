10e2d0155e2969f70f1448eec8e946bf67a922e4
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-05-14 17:27:26 +0800

    Create 454.4sum-ii.cpp

---
+++ leetcode/454.4sum-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int,int> map;
+        int cnt = 0;
+        for(int i : A){
+            for(int j : B){
+                //写法并不推荐 但是简单
+                map[i + j]++;//如果不存在键的话，会首先创建键并初始化值为零 ， 再++ 
+            }
+        }
+        
+        for(int i : C){
+            for(int j : D){
+                if(map.find(-(i+j)) != map.end()){
+                    cnt += map[-(i+j)];
+                }
+            }
+        }
+        
+        return cnt;
+    }
+};
