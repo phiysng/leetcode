c1f5d15a4e78ca0fdcd7ce34009af74848ec72c9
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-05-23 17:46:02 +0800

    add numberOf1.

--- coding-interview/README.md
+++ coding-interview/README.md
@@ -7,3 +7,4 @@
 - 6 [从尾到头打印链表](./printListFromTailToHead.cpp)
 - 10 [斐波那契额数列](./Fibonacci.cpp) [青蛙跳台阶](./jumpFloor.cpp)
 - 14 [剪绳子](./maxProductAfterCutting.cpp)
+- 15[二进制中1的个数](./numberOf1.cpp) 
---
+++ coding-interview/numberOf1.cpp
@@ -0,0 +1,30 @@
+///面试题 15 二进制中1的个数
+
+class Solution
+{
+public:
+    int NumberOf1(int n)
+    {
+        int count = 0;
+        unsigned int flag = 1; //只有最低位位1
+        while (flag)
+        { //循环直到1被移除
+            if (n & flag)
+                ++count;
+            flag <<= 1;
+        }
+        return count;
+    }
+    int NumberOf1_v2(int n)
+    {
+        int count = 0;
+        while (n)
+        {
+            ++count; //数字n还有位为1
+            n = n & (n - 1);
+            /// n-1会将最右面的1变为0，接着到最低位全为一
+            /// 这条语句清楚了n最左边的零
+        }
+        return count;
+    }
+};
