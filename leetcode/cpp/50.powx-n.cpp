80b0d4c08065475500eddc59ee42c56b459ce381
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-07-12 21:47:32 +0800

    add leetcode solutions.
    add
    - 50
    - 66

---
+++ leetcode/cpp/50.powx-n.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n)
+    {
+        int64_t n_ = abs(static_cast<int64_t>(n));
+        // x == 0 时怎么计算
+        if (n == 0)
+            return 1;
+        double res = fastPow(x, n_);
+        return n > 0 ? res : 1 / res;
+    }
+    
+    // 快速幂运算
+    double fastPow(double x, int64_t n)
+    {
+        if (n == 1)
+            return x;
+        if (n == 2)
+            return x * x;
+        double t = fastPow(x, n / 2);
+        t *= t;
+        if (n % 2 == 1) {
+            t *= x;
+        }
+        return t;
+    }
+};
---
+++ leetcode/cpp/66.plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits)
+    {
+        int carry = 0;
+        deque<int> res;
+        digits[digits.size() - 1]++;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            int t = digits[i];
+            t = carry ? t + 1 : t;
+            carry = t / 10;
+            t = t % 10;
+            res.push_front(t);
+        }
+        if (carry)
+            res.push_front(1);
+        return vector<int>(res.begin(), res.end());
+    }
+};
