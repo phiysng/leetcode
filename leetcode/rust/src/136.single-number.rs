e3f32bf99365c24dd8dc2e87d2703c6e1e277055
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-08-24 09:09:14 +0800

    add leetcode rust.
    add
    - 136

---
+++ leetcode/rust/src/136.single-number.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        
+        let mut res = 0;
+
+        for i in nums{
+            res ^= i;
+        }
+        res
+    }
+}
