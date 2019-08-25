e2677a5da4cc80c6dabe82475bb4bf0ed7791b84
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-08-25 22:45:30 +0800

    add leetcode 34.

---
+++ leetcode/cpp/34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target)
+    {
+        if (nums.size() == 0)
+            return { -1, -1 };
+
+        // 两次二分查找
+
+        int l = 0, r = nums.size() - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        if (nums[l] != target)
+            return { -1, -1 };
+        int lres = l;
+
+        r = nums.size();
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] <= target) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+
+        int rres = l - 1;
+        return { lres, rres };
+    }
+};
