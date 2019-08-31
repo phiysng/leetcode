dc66d7be448209ec3f799c748894ac498426ccae
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-08-31 13:54:27 +0800

    add leetcode 455.

---
+++ leetcode/cpp/455.assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s)
+    {
+        sort(begin(g), end(g));
+        sort(begin(s), end(s));
+
+        int i = 0;
+        int j = 0;
+        // 将小的饼干分给更容易满足的孩子
+        while (i < g.size() && j < s.size()) {
+            if (g[i] <= s[j]) {
+                ++i;
+            }
+            ++j;
+        }
+        return i;
+    }
+};
--- leetcode/cpp/75.sort-colors.cpp
+++ leetcode/cpp/75.sort-colors.cpp
@@ -2,19 +2,27 @@ class Solution {
 public:
     void sortColors(vector<int>& nums)
     {
-        int zero = -1;
-        int two = nums.size();
-        int i = 0;
-        while (i < two) {
-            if (nums[i] == 0) {
-                swap(nums[i], nums[++zero]);
-                ++i;
-                continue;
+        // 两个指针分别指向 0 || 2 当前的边界
+        int zero = -1; //左边及 zero 都是0
+        int two = nums.size(); //右边及two都是2
+        int one = 0;
+
+        while (one < two) {
+            if (nums[one] == 0) {
+                // 左边是00011这种
+                // 将一个1 与 这个0交换
+                ++zero;
+                swap(nums[zero], nums[one]);
+                ++one;
             } else {
-                if (nums[i] == 2) {
-                    swap(nums[i], nums[--two]);
+                if (nums[one] == 2) {
+                    //交换过来的可能是0 / 1 不能直接指向下一个节点
+                    --two;
+                    swap(nums[two], nums[one]);
+
                 } else {
-                    ++i;
+                    // 左边肯定都是1
+                    ++one;
                 }
             }
         }
