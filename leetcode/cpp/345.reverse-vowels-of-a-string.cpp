1f0e2babbdd534f21c1889b0d4be72884e1a1f7a
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-08-31 09:21:34 +0800

    add leetcode.

---
+++ leetcode/cpp/345.reverse-vowels-of-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int l = 0 , r = s.size() -1;
+        unordered_set<char> set{'a','e','i','o','u','A','E','I','O','U'};
+        while( l < r){
+            while(l < r && set.find(s[l]) == set.end()){
+                ++l;
+            }
+            
+            while(l < r && set.find(s[r]) == set.end()){
+                --r;
+            }
+            
+            if(l >= r){
+                break;
+            }
+            else{
+                swap(s[l],s[r]);
+                ++l;
+                --r;
+            }
+        }
+        return s;
+    }
+};
---
+++ leetcode/cpp/633.sum-of-square-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+
+        if(c <= 2) return true;
+        
+        int64_t l = 0 , r = sqrt(c);
+        
+        while(l <= r){
+            int64_t t = l*l + r*r;
+            if(t == c){
+                return true;
+            }
+            if(t > c){
+                --r;
+            }
+            else{
+                ++l;
+            }
+        }
+        
+        return false;
+    }
+};
