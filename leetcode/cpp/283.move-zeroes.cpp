594db2f2fdcd8437bda61f39ffff115f26fa41b9
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-09-05 09:41:10 +0800

    Create 283.move-zeroes.cpp

---
+++ leetcode/cpp/283.move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int idx = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[i] != 0){
+                nums[idx++] = nums[i];
+            }
+        }
+        for(int i = idx; i <nums.size();++i){
+            nums[i] = 0;
+        }
+    }
+};
