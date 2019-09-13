0a419656d7dd79e0aede3bbd9e52012fb3344f4a
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-09-13 22:19:16 +0800

    add leetcode solutions.
    add
    - 169

---
+++ leetcode/cpp/169.majority-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // 求众数
+    int majorityElement(vector<int>& nums)
+    {
+        int n = 1;
+
+        int curr = nums[0];
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == curr) {
+                ++n;
+            } else {
+                --n;
+                if (n == -1) {
+                    n = 1;
+                    curr = nums[i];
+                }
+            }
+        }
+        return curr;
+    }
+};
