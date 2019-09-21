ebc07c94f0bf82bbc5ab40f14ce350082a238ec2
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-09-21 21:31:56 +0800

    optimize leetcode 64.
    优化空间复杂度至O(1).

--- leetcode/cpp/64.minimum-path-sum.cpp
+++ leetcode/cpp/64.minimum-path-sum.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid)
-    {
-        if (grid.size() == 0)
-            return 0;
-        vector<vector<int>> v(grid.size(), vector<int>(grid[0].size()));
-
-        v[0][0] = grid[0][0];
-
-        for (int i = 1; i < grid[0].size(); ++i) {
-            v[0][i] = v[0][i - 1] + grid[0][i];
-        }
-        for (int i = 1; i < grid.size(); ++i) {
-            v[i][0] = grid[i][0] + v[i - 1][0];
-        }
-
-        for (int i = 1; i < grid.size(); ++i) {
-            for (int j = 1; j < grid[0].size(); ++j) {
-                v[i][j] = min(v[i][j - 1], v[i - 1][j]) + grid[i][j];
+    int minPathSum(vector<vector<int>>& grid) {
+        if(grid.size() == 0) return 0;
+        
+        for(int i = 0; i < grid.size();++i){
+            for(int j = 0; j < grid[0].size();++j){
+                if(i == 0 && j == 0) continue;
+                if(i == 0) grid[0][j] += grid[0][j-1];
+                if(j == 0) grid[i][0] += grid[i-1][0];
+                if(i != 0 && j != 0)
+                {
+                    grid[i][j] += min(grid[i][j-1],grid[i-1][j]);
+                }
             }
         }
-        return v.back().back();
+        
+        return grid.back().back();
     }
 };
