cecc0ead74f42bd789c61664976d50efd803f6d7
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-09-21 23:25:44 +0800

    add leetcode .
    add
    - 63
    - 746 优化至O(1)空间复杂度

---
+++ leetcode/cpp/63.unique-paths-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
+    {
+        if (obstacleGrid.size() == 0 || obstacleGrid[0].size() == 0)
+            return 0;
+
+        vector<int64_t> v(obstacleGrid[0].size());
+
+        for (int i = 0; i < obstacleGrid.size(); ++i) {
+            for (int j = 0; j < obstacleGrid[0].size(); ++j) {
+                // [0][0]时 如果grid[0][0] == 1的话 可以直接返回0
+                if (i == 0 && j == 0) {
+                    if (obstacleGrid[0][0] == 1) {
+                        v[0] = 0;
+                    } else {
+                        v[0] = 1;
+                    }
+                    continue;
+                }
+                if (obstacleGrid[i][j] == 1) {
+                    v[j] = 0;
+                    continue;
+                }
+                if (i == 0) {
+                    if (j > 0 && obstacleGrid[0][j - 1] == 1) {
+                        v[j] = 0;
+                    } else {
+                        v[j] = v[j - 1];
+                    }
+                    continue;
+                }
+
+                if (j == 0) {
+
+                    if (obstacleGrid[i - 1][0] == 1) {
+                        v[j] = 0;
+                    }
+                    continue;
+                }
+
+                int t = 0;
+                if (obstacleGrid[i][j - 1] == 0) {
+                    t += v[j - 1];
+                }
+                if (obstacleGrid[i - 1][j] == 0) {
+                    t += v[j];
+                }
+                v[j] = t;
+            }
+        }
+
+        return v.back();
+    }
+};
--- leetcode/cpp/746.min-cost-climbing-stairs.cpp
+++ leetcode/cpp/746.min-cost-climbing-stairs.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost)
-    {
-        vector<int> v(cost.size() + 1);
-        v[0] = cost[0];
-        v[1] = cost[1];
-        for (int i = 2; i < cost.size(); ++i) {
-            v[i] = min(v[i - 2] + cost[i], v[i - 1] + cost[i]);
+    int minCostClimbingStairs(vector<int>& cost) {
+        int costs = 0;
+        if(cost.size() <= 2) return min(cost[0],cost[1]);
+        
+        int prev = cost[0] , curr = cost[1];
+        for(int i = 2; i < cost.size();++i){
+            int t = min(prev , curr);
+            t += cost[i];
+            prev = curr;
+            curr = t;
         }
-        return min(v[cost.size() - 1], v[cost.size() - 2]);
+        
+        return min(curr,prev);
     }
 };
