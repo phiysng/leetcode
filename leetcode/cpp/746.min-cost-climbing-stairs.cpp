695282b1024803efac51c3f0f0de3b62122a2b8a
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-09-21 23:26:46 +0800

    format cpp file.

--- leetcode/cpp/746.min-cost-climbing-stairs.cpp
+++ leetcode/cpp/746.min-cost-climbing-stairs.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
+    int minCostClimbingStairs(vector<int>& cost)
+    {
         int costs = 0;
-        if(cost.size() <= 2) return min(cost[0],cost[1]);
-        
-        int prev = cost[0] , curr = cost[1];
-        for(int i = 2; i < cost.size();++i){
-            int t = min(prev , curr);
+        if (cost.size() <= 2)
+            return min(cost[0], cost[1]);
+
+        int prev = cost[0], curr = cost[1];
+        for (int i = 2; i < cost.size(); ++i) {
+            int t = min(prev, curr);
             t += cost[i];
             prev = curr;
             curr = t;
         }
-        
-        return min(curr,prev);
+
+        return min(curr, prev);
     }
 };
