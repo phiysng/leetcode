14215c92936ef62676b4961a0339e11a9f4fe6c4
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-09-21 21:42:36 +0800

    add leetcode dp.
    add
    - 62 O(m) space

---
+++ leetcode/cpp/62.unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n)
+    {
+        vector<int> v(m, 1); // 第一行
+
+        for (int i = 0; i < n - 1; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (j == 0) {
+                    continue;
+                } else {
+                    v[j] += v[j - 1];
+                }
+            }
+        }
+        return v.back();
+    }
+};
--- leetcode/cpp/64.minimum-path-sum.cpp
+++ leetcode/cpp/64.minimum-path-sum.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        if(grid.size() == 0) return 0;
-        
-        for(int i = 0; i < grid.size();++i){
-            for(int j = 0; j < grid[0].size();++j){
-                if(i == 0 && j == 0) continue;
-                if(i == 0) grid[0][j] += grid[0][j-1];
-                if(j == 0) grid[i][0] += grid[i-1][0];
-                if(i != 0 && j != 0)
-                {
-                    grid[i][j] += min(grid[i][j-1],grid[i-1][j]);
+    int minPathSum(vector<vector<int>>& grid)
+    {
+        if (grid.size() == 0)
+            return 0;
+
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (i == 0 && j == 0)
+                    continue;
+                if (i == 0)
+                    grid[0][j] += grid[0][j - 1];
+                if (j == 0)
+                    grid[i][0] += grid[i - 1][0];
+                if (i != 0 && j != 0) {
+                    grid[i][j] += min(grid[i][j - 1], grid[i - 1][j]);
                 }
             }
         }
-        
+
         return grid.back().back();
     }
 };
