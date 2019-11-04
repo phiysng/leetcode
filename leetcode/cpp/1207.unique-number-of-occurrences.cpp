34535c699938021b13d08ca5c03e684c26277245
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-11-04 23:35:35 +0800

    add leetcode 1207.

---
+++ leetcode/cpp/1207.unique-number-of-occurrences.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    bool uniqueOccurrences(vector<int> &arr)
+    {
+        unordered_map<int, int> map;
+        for (int i : arr)
+        {
+            map[i]++;
+        }
+        unordered_set<int> set;
+        for (auto &p : map)
+        {
+            if (set.find(p.second) == set.end())
+            {
+                set.insert(p.second);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
