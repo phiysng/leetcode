b7e850f5cc935699693a48d3c05ffd2797a8ed62
Wu Yuanshou <wuyuanshou@protonmail.com>
2019-12-10 16:06:00 +0800

    update more rusty statements.

--- leetcode/rust/src/198.house-robber.rs
+++ leetcode/rust/src/198.house-robber.rs
@@ -3,21 +3,18 @@ use std::cmp::max;
 
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        
-        if(nums.len() == 0){ return 0;}
-        
-        let mut v = Vec::new();
-        v.push(nums[0]);
-        let m = *nums.first().unwrap();
+        if nums.is_empty() { return 0; }
 
-        for i in 1..nums.len() {
-            let mut has_n = nums[i];
-            if i > 1{
-                has_n +=   v[i - 2];
-            }
+        if nums.len() == 1 {
+            return nums[0];
+        }
+
+        let mut v = vec![nums[0], max(nums[0], nums[1])];
+
+        for i in 2..nums.len() {
+            let mut has_n = nums[i] + v[i - 2];
             let dont = v[i - 1];
             v.push(max(has_n, dont));
-
         }
 
         *v.last().unwrap()
