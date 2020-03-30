4c116cc8e822de679ca291c43234dc96528a46bb
Wu Yuanshou <wuyuanshou@protonmail.com>
2020-03-30 15:40:30 +0800

    update header with ListNode.

--- leetcode/cpp/oj_header.h
+++ leetcode/cpp/oj_header.h
@@ -11,15 +11,22 @@
 #include <unordered_set>
 
 using namespace std;
-struct TreeNode {
+struct TreeNode
+{
     int val;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
     TreeNode(int x)
-        : val(x)
-        , left(nullptr)
-        , right(nullptr)
+        : val(x), left(nullptr), right(nullptr)
     {
     }
 };
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 #endif // OJ_H
