2abff191dff538410e4d936bf15b6bb9f10f6be6
Wu Yuanshou <wuyuanshou@protonmail.com>
2020-03-31 19:32:39 +0800

    fix cppcheck warning: use std::accumulate.

--- leetcode/cpp/477.total-hamming-distance.cpp
+++ leetcode/cpp/477.total-hamming-distance.cpp
@@ -5,7 +5,6 @@ class Solution
 public:
     int totalHammingDistance(vector<int> &nums)
     {
-        int res = 0;
         vector<int> bits(32);
 
         for (int val : nums)
@@ -21,11 +20,8 @@ public:
 
         const int _size = nums.size();
 
-        for (auto &bit : bits)
-        {
-            res += bit * (_size - bit);
-        }
-
-        return res;
+        return std::accumulate(begin(bits),end(bits),0,[=](int bit){
+            return bit * (_size - bit);
+        });
     }
 };
--- leetcode/cpp/oj_header.h
+++ leetcode/cpp/oj_header.h
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <queue>
 #include <stack>
+#include <numeric>
 #include <unordered_map>
 #include <unordered_set>
 
