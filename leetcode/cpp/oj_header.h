1777c57e1eabffc1ab699b01d0d3a027106539ed
Wu Yuanshou <wuyuanshou@protonmail.com>
2020-03-31 19:26:24 +0800

    fix cppccheck warnings.

--- leetcode/cpp/oj_header.h
+++ leetcode/cpp/oj_header.h
@@ -16,7 +16,7 @@ struct TreeNode
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x)
+    explicit TreeNode(int x)
         : val(x), left(nullptr), right(nullptr)
     {
     }
@@ -26,7 +26,7 @@ struct ListNode
 {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    explicit ListNode(int x) : val(x), next(NULL) {}
 };
 
 #endif // OJ_H
