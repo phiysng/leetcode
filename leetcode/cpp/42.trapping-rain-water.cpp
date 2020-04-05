f677f9e62486c0311f1d4ddbe6662edad208064d
Wu Yuanshou <wuyuanshou@protonmail.com>
2020-04-05 09:42:10 +0800

    update leetcode 42.
    contribute a more clear impl.

--- leetcode/cpp/42.trapping-rain-water.cpp
+++ leetcode/cpp/42.trapping-rain-water.cpp
@@ -1,6 +1,71 @@
-class Solution {
+#include "oj_header.h"
+
+/// refactor version.
+class Solution
+{
 public:
-    int trap(vector<int>& height)
+    int trap(vector<int> &height)
+    {
+        if (height.size() <= 1)
+            return 0;
+
+        vector<int> se_stack{0}; // sequence queue.
+
+        int res = 0;
+
+        /// stack shall at least have one element.
+        for (int i = 1; i < height.size(); ++i)
+        {
+            if (height[i] <= height[se_stack.back()])
+            {
+                se_stack.push_back(i);
+            }
+            else
+            {
+                int _size = se_stack.size();
+                for (int k = _size - 2; k >= 0; --k)
+                {
+                    int dis = i - se_stack[k] - 1;
+                    int space = dis * (min<int>(height[se_stack[k]], height[i]) - height[se_stack[k + 1]]);
+                    res += space;
+
+                    se_stack.pop_back();
+                    if (height[se_stack.back()] > height[i])
+                    {
+                        break;
+                    }
+                }
+                /// under specific condition the loop will break
+                /// - the size of se_stack is less than 2.
+                /// - the height of se_stack.back() >= height[i]
+                ///     - in which time the size of se_stack can be 1 also.
+                if (height[se_stack.back()] >= height[i])
+                {
+                    se_stack.push_back(i);
+                    continue;
+                }
+                else
+                {
+                    /// the se_stack must either have one item greater than height[i]
+                    /// or have all item less than height[i] , in which case the size
+                    /// of se_stack is 1.
+                    /// assert(se_stack.size() == 1);
+                    /// replace the last one.
+                    /// FIMXE: when use `se_stack.back()` instead , the speed is dramatically slow
+                    se_stack.pop_back();
+                    se_stack.push_back(i);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+/// borrowed from leetcode.
+class Solution
+{
+public:
+    int trap(vector<int> &height)
     {
         //顺序栈、
         // 使用一个栈，每次遇到一个比栈顶元素更大的数的时候，
@@ -12,15 +77,17 @@ public:
 
         int res = 0;
 
-        for (size_t i = 0; i < height.size(); i++) {
+        for (size_t i = 0; i < height.size(); i++)
+        {
             // 寻找所有的小于height[curr]的点
-            while (!s.empty() && height[i] > height[s.top()]) {
+            while (!s.empty() && height[i] > height[s.top()])
+            {
                 int t = s.top();
                 s.pop();
                 if (s.empty())
                     break;
 
-                int prev = s.top(); //左边的边界的位置
+                int prev = s.top();                   //左边的边界的位置
                 int h = min(height[i], height[prev]); // 当前两边的最低边减去底边
                 int dis = i - prev - 1;
                 res += dis * (h - height[t]);
