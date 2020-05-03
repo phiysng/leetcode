7f011080cb02116169786684be013354f4b72cb1
Wu Yuanshou <wuyuanshou@protonmail.com>
2020-05-03 21:18:41 +0800

    add leetcode 53 and suppress code's intellisense.

--- leetcode/cpp/41.first-missing-positive.cpp
+++ leetcode/cpp/41.first-missing-positive.cpp
@@ -1,16 +1,21 @@
-class Solution {
+#include "oj_header.h"
+class Solution
+{
 public:
     //用nums[0..n) 存储[1..n+1)
     //看第一个不符合的数是哪一个
-    int firstMissingPositive(vector<int>& nums)
+    int firstMissingPositive(vector<int> &nums)
     {
-        for (int i = 0; i < nums.size(); i++) {
-            while (nums[i] > 0 && nums[i] <= nums.size() && nums[i] != nums[nums[i] - 1]) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            while (nums[i] > 0 && nums[i] <= nums.size() && nums[i] != nums[nums[i] - 1])
+            {
                 swap(nums[i], nums[nums[i] - 1]);
             }
         }
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
+        {
             if (nums[i] != i + 1)
                 return i + 1;
         }
--- leetcode/cpp/53.maximum-subarray.cpp
+++ leetcode/cpp/53.maximum-subarray.cpp
@@ -1,7 +1,24 @@
-// #include"oj_header.h"
-class Solution {
+#include "oj_header.h"
+class Solution
+{
 public:
-    int maxSubArray(vector<int>& nums)
+    int maxSubArray(vector<int> &nums)
     {
+        if (nums.size() == 0)
+            return 0;
+        if (nums.size() == 1)
+            return nums[0];
+        int m = nums[0];
+        int prev = m;
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            if (prev < 0)
+                prev = nums[i];
+            else
+                prev += nums[i];
+            m = max<int>(prev, m);
+        }
+
+        return m;
     }
 };
