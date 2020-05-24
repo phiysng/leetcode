812ae41ca4314754c440739ccc39113fbe92411a
Wu Yuanshou <wuyuanshou@protonmail.com>
2020-05-24 18:59:25 +0800

    minor optimise for lc 5418.

--- leetcode/python/5418.pseudo-palindromic-paths-in-a-binary-tree.py
+++ leetcode/python/5418.pseudo-palindromic-paths-in-a-binary-tree.py
@@ -6,43 +6,40 @@
 #         self.right = right
 from util import TreeNode
 from typing import List
+from typing import Dict
 
 
 class Solution:
     def pseudoPalindromicPaths(self, root: TreeNode) -> int:
         r = [0]
+        d = {}
+        for i in range(1, 10):
+            d[i] = 0
 
-        self.back_track(root, [], r)
+        self.back_track(root, d, r)
 
         return r[0]
 
-    def back_track(self, root: TreeNode, values: List[int], res: List[int]):
+    def back_track(self, root: TreeNode, m: Dict[int, int], res: List[int]):
 
         if self.isLeaf(root):
-            values.append(root.val)
-            if self.checkValidPseudo(values):
+            m[root.val] += 1
+            if self.checkValidPseudo(m):
                 res[0] += 1
-            values.pop()
-
+            m[root.val] -= 1
             return
-        values.append(root.val)
+
+        m[root.val] += 1
         if root.left:
-            self.back_track(root.left, values, res)
+            self.back_track(root.left, m, res)
         if root.right:
-            self.back_track(root.right, values, res)
+            self.back_track(root.right, m, res)
 
-        values.pop()
+        m[root.val] -= 1
 
-    def checkValidPseudo(self, values: List[int]):
-        m = {}
-        for i in range(1, 10):
-            m[i] = 0
-
-        for v in values:
-            m[v] += 1
+    def checkValidPseudo(self, m: Dict[int, int]):
 
         not_even = 0
-
         for key in m:
             if m[key] % 2 == 1:
                 not_even += 1
