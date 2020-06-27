90691196391878203b632a5e0d23096c7a15d4e9
Wu Yuanshou <wuyuanshou@protonmail.com>
2020-06-27 21:33:20 +0800

    lc 1488 with another approach.

--- leetcode/java/1488.avoid-flood-in-the-city.java
+++ leetcode/java/1488.avoid-flood-in-the-city.java
@@ -8,8 +8,8 @@ import java.util.PriorityQueue;
 import java.util.Set;
 
 /**
- * 在没下雨的那一天就要决定进行抽水 
- * 否则如果遇到满的湖再次下雨再决定没下雨的那天抽那个湖就需要处理时间区间上的问题
+ * 其他人的解法真的快
+ * 在没下雨的那一天就要决定进行抽水 否则如果遇到满的湖再次下雨再决定没下雨的那天抽那个湖就需要处理时间区间上的问题 
  */
 class Solution {
     public int[] avoidFlood(int[] rains) {
@@ -63,5 +63,96 @@ class Solution {
             }
         }
         return r.stream().mapToInt(i -> i).toArray();
+    }
+}
+
+/**
+ * 原来的思路 在湖满之后再下雨才处理之前的抽水 二分查找
+ */
+class Solution_v2 {
+    public int[] avoidFlood(int[] rains) {
+        List<Integer> drains = new LinkedList<>();
+        List<Integer> r = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < rains.length; i++) {
+            if (rains[i] > 0) {
+                if (set.contains(rains[i])) {
+                    if (drains.isEmpty()) {
+                        return new int[0];
+                    }
+
+                    int idx = map.get(rains[i]);
+                    System.out.println(idx);
+                    int rcode = binarySearch(drains, idx);
+                    if (rcode == -1) {
+                        return new int[0];
+                    }
+                    r.set(drains.get(rcode), rains[i]);
+                    drains.remove(rcode);
+
+                } else {
+                    // 之前为空湖
+                    set.add(rains[i]);
+                }
+                r.add(-1);
+                map.put(rains[i], i);
+            } else {
+                drains.add(i);
+                r.add(1);
+            }
+        }
+        return r.stream().mapToInt(i -> i).toArray();
+    }
+
+    private static int BruteForceSearch(List<Integer> list, int limit) {
+        if (list.get(0) > limit)
+            return 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) < limit && list.get(i + 1) > limit)
+                return i + 1;
+        }
+        return -1;
+    }
+
+    private static int binarySearch(List<Integer> list, int limit) {
+
+        int l = 0;
+        int r = list.size();
+        if (list.isEmpty())
+            return -1;
+        if (list.size() == 1) {
+            return list.get(0) > limit ? 0 : -1;
+        }
+
+        if (list.get(0).compareTo(limit) > 0)
+            return 0;
+
+        if (list.get(r - 1).compareTo(limit) < 0)
+            return -1;
+
+        while (l < r) {
+            int mid = (l + r) / 2;
+
+            if (mid == 0 && list.get(mid).compareTo(limit) < 0 && list.get(mid + 1).compareTo(limit) > 0)
+                return 1;
+
+            if (mid == r - 1 && list.get(mid).compareTo(limit) > 0 && list.get(mid - 1).compareTo(limit) < 0)
+                return mid;
+
+            if (list.get(mid).compareTo(limit) < 0 && mid + 1 < r && list.get(mid + 1).compareTo(limit) > 0) {
+                return mid + 1;
+            }
+            if (list.get(mid).compareTo(limit) < 0 && mid + 1 < r && list.get(mid + 1).compareTo(limit) < 0) {
+                l = mid + 1;
+            }
+            if (list.get(mid).compareTo(limit) > 0 && mid + 1 < r && list.get(mid + 1).compareTo(limit) > 0) {
+                r = mid + 1;
+            }
+
+        }
+
+        return -1;
     }
 }
