e0a9f1b74b675844da3a081c0a5c0f5af15d06d6
Wu Yuanshou <wuyuanshou@protonmail.com>
2022-05-02 21:00:38 +0800

    fix build error.

--- leetcode/cpp/2248.intersection-of-multiple-arrays.cpp
+++ leetcode/cpp/2248.intersection-of-multiple-arrays.cpp
@@ -1,26 +1,28 @@
+#include"oj_header.h"
+
 class Solution {
 public:
-    vector<int> intersection(vector<vector<int>>& nums) {
-        unordered_map<int, int> map;
-        for (auto& num_list : nums)
-        {
-            for (auto& n : num_list)
-            {
-                map[n]++;
-            }
-        }
+	vector<int> intersection(vector<vector<int>>& nums) {
+		unordered_map<int, int> map;
+		for (auto& num_list : nums)
+		{
+			for (auto& n : num_list)
+			{
+				map[n]++;
+			}
+		}
 
-        vector<int> r;
+		vector<int> r;
 
-        for (auto& k : map)
-        {
-            if (k.second == nums.size())
-            {
-                r.push_back(k.first);
-            }
-        }
+		for (auto& k : map)
+		{
+			if (k.second == nums.size())
+			{
+				r.push_back(k.first);
+			}
+		}
 
-        sort(begin(r), end(r));
-        return r;
-    }
+		sort(begin(r), end(r));
+		return r;
+	}
 };
